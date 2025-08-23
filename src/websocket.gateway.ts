79b3f4b53af3a71c3341a2be6d295478a78f4d2f
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-08-23 12:23:11 +0200

    added websocket

---
+++ src/websocket.gateway.ts
@@ -0,0 +1,36 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+@WebSocketGateway({
+  cors: { origin: '*' },
+})
+export class WebsocketGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
+
+  afterInit() {
+    console.log('WebSocket initialized');
+  }
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Client disconnected:', client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(@MessageBody() message: string) {
+    console.log('Got message:', message);
+    this.server.emit('message', `Echo: ${message}`);
+  }
+}
